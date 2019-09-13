7494fc4bb606ebdce51406a419a9ad4732e36182
luiseduardiazc <luis.diaz.car@gmail.com>
2019-09-13 00:22:24 +0000

    make not be executed when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import builtins
-import hidden_4
-names = dir(hidden_4)
+if __name__ == '__main__':
 
-print_names = []
-for name in names:
-    if not name.startswith('__'):
-        print_names.append(name)
-print_names.sort()
-for names in print_names:
-    print("{}".format(names))
+    import builtins
+    import hidden_4
+    names = dir(hidden_4)
+
+    print_names = []
+    for name in names:
+        if not name.startswith('__'):
+            print_names.append(name)
+    print_names.sort()
+    for names in print_names:
+        print("{}".format(names))
