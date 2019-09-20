af82cc6d7403191a63b359fef495aaced944ef39
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-09-20 00:11:08 -0500

    fixed typo

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_list = [list(map(lambda x: x ** 2, row)) for row in matrix]
-    return new_list
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
