67bfbe4ece010bc06d943049f12928b6be9a1a8a
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-09-30 13:41:49 -0500

    convert string to int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    is_number = True
     try:
-        print("{:d}".format(value))
-        return True
+        print("{:d}".format(int(value)))
     except ValueError:
-        return False
-
+        is_number = False
+    finally:
+        return is_number
