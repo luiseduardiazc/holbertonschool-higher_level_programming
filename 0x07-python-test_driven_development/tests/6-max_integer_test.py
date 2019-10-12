cf62acb61486ba062c648450cd4b0054a71d45df
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-10-11 19:12:12 -0500

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,30 +6,67 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """Class for test"""
 
-    def test_send_list(self):
-        result = max_integer([1, 2, 3, 4])
-        self.assertEqual(result, 4)
+    def test_numbers(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer([]), None)
 
-    def test_empty_list(self):
-        result = max_integer([])
-        self.assertIsNone(result)
+    def test_1(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer([752]), 752)
 
-    def test_negative_numbers(self):
-        result = max_integer([-50, -10, -5, -2, -1, 0])
-        self.assertEqual(result, 0)
-        result = max_integer([-50, -10, -5])
-        self.assertEqual(result, -5)
+    def test_2(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer(), None)
 
-    def test_same_numbers(self):
-        result = max_integer([200, 200, 200, 200, 200, 200])
-        self.assertEqual(result, 200)
-        result = max_integer([0, 0, 0, 0])
-        self.assertEqual(result, 0)
+    def test_3(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer([5, -15843, 7, 9456, 752]), 9456)
 
-    def test_float_numbers(self):
-        result = max_integer([0.1, 0.2, 0.5, -0.4, 0.02])
-        self.assertEqual(result, 0.5)
+    def test_4(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer([-5, -15843, -7, -9456, -752]), -5)
+
+    def test_5(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer([0, -15843, 7, 9456, 752]), 9456)
+
+    def test_6(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer([3, 3, 3, 3, 3]), 3)
+
+    def test_7(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer('zapato'), 'z')
+
+    def test_8(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_9(self):
+        """Test with numbers"""
+        with self.assertRaises(TypeError):
+            max_integer([(1, 67, 3, 5), "hello"])
+
+    def test_A(self):
+        """Test with numbers"""
+        with self.assertRaises(TypeError):
+            max_integer([[5, 5], [79], '89', [75]])
+
+    def test_B(self):
+        """Test with numbers"""
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_C(self):
+        """Test with numbers"""
+        with self.assertRaises(TypeError):
+            max_integer([5, 5, 79, '89', 75])
+
+    def test_D(self):
+        """Test with numbers"""
+        self.assertEqual(max_integer([0, -15, 7, 94, 752, 645631]), 645631)
 
 
 if __name__ == '__main__':
