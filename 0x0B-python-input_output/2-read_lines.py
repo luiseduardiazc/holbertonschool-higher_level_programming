098229965ef76abc3f7c80c15759299588954a30
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-10-16 22:27:54 -0500

    refactor code

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,10 +5,14 @@
 def read_lines(filename="", nb_lines=0):
     """ Function that reads n lines of a text file (UTF8)
         and prints it to stdout """
+    num_lines = 0
     with open(filename, 'r', encoding='UTF8') as file:
         if nb_lines < 1:
             for line in file:
                 print(line)
+                num_lines += 1
         else:
             for x in range(nb_lines):
                 print(file.readline())
+                num_lines += 1
+    return num_lines
