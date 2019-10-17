652fda5f48736714161937a2830087b0144c1f68
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-10-16 22:10:59 -0500

    fixed encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ Read file """
-    with open(filename, 'r', encoding='UTF8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line)
