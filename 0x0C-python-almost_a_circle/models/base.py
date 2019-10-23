6c99aeed34eb180c41048fcf006df22999f407a2
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-10-22 23:30:33 -0500

    add csv task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -67,3 +67,29 @@ class Base:
         for index, e in enumerate(list_instances):
             list_instances[index] = cls.create(**list_instances[index])
         return list_instances
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """writes the JSON string representation
+            of list_objs to a csv"""
+        file_name = str(cls.__name__) + ".csv"
+        list_dict = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_dict.append(obj.to_dictionary())
+
+        with open(file_name, 'w') as writer:
+            writer.write(cls.to_json_string(list_dict))
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ returns a list of instances """
+        file_name = cls.__name__ + ".csv"
+        list_instances = []
+        if not os.path.isfile(file_name):
+            return list_instances
+        with open(file_name, 'r') as reader:
+            list_instances = cls.from_json_string(reader.read())
+        for index, e in enumerate(list_instances):
+            list_instances[index] = cls.create(**list_instances[index])
+        return list_instances
