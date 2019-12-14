d93be79222c5e75163e6a5e80bcd1981d43eefb3
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-14 17:39:46 -0500

    add task 2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+'''
+module for states
+'''
+_SQL = """ SELECT * FROM states
+            WHERE  states.name LIKE 'N%'
+            ORDER BY states.id"""
+
+
+def get_db_connexion(**kwargs):
+    '''
+    get database connexion
+    '''
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           charset="utf8", **kwargs)
+    return conn
+
+
+def get_all_states(conn):
+    '''
+    lists all states with a name starting with N
+    print all states order by id
+    '''
+    cur = conn.cursor()
+    cur.execute(_SQL)
+    query_rows = cur.fetchall()
+    for item in query_rows:
+        print(item)
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    get_all_states(get_db_connexion(user=user, passwd=passwd, db=db))
