2f3251096af01ad56342c8f5eed55144cb97b4b1
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-14 18:15:10 -0500

    add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+'''
+module for states
+'''
+_SQL = """ SELECT * FROM states
+            WHERE  states.name = '{}'
+            ORDER BY states.id ASC"""
+
+
+def get_db_connexion(**kwargs):
+    '''
+    get database connexion
+    '''
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           charset="utf8", **kwargs)
+    return conn
+
+
+def get_all_states(conn, state_name):
+    '''
+    displays all values in the states table of hbtn_0e_0_usa
+    where name matches the argument
+    '''
+    cur = conn.cursor()
+    cur.execute(_SQL.format(state_name))
+    query_rows = cur.fetchall()
+    for item in query_rows:
+        print(item)
+    cur.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    state_name = sys.argv[4]
+    get_all_states(get_db_connexion(
+        user=user, passwd=passwd, db=db), state_name)
