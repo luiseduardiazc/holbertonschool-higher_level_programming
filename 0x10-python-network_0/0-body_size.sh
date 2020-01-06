797d6b0a17d844c9371cde539b2a122cb9794123
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-06 16:06:29 -0500

    fixed

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-curl -sI "$1" | awk -v FS=": " '/^Content-Length/{print $2}'
+curl -so /dev/null "$1" -w '%{size_download}'
