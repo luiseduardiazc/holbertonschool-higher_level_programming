d30be7201d97993506f4e8d248a87368aa0986b0
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-06 21:23:01 -0500

    fixed again

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sd -d "email=hr%40holbertonschool%2Ecom&subject=I+will+always+be+here+for+PLD" -X POST "$1"
+curl "$1" -s -X POST -d "email=hr%40holbertonschool%2Ecom&subject=I+will+always+be+here+for+PLD"
