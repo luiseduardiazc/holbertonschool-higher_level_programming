e99ec9c2bd1e559b8bc4ed5ee1a491434a8c6ddd
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-07 11:27:57 -0500

    create merge sort algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,15 +2,43 @@
 ''' Find a peak '''
 
 
+def merge(L1, L2, Li):
+    '''
+    Merge two sorted python list L1 and L2
+    into properly sized list Li
+    '''
+    i = j = 0
+    while i + j < len(Li):
+        if j == len(L2) or (i < len(L1) and L1[i] < L2[j]):
+            Li[i+j] = L1[i]
+            i += 1
+        else:
+            Li[i+j] = L2[j]
+            j += 1
+
+
+def merge_sort(Li):
+    '''
+    Sort the elements of Python list Li
+    using the merge-sort algorithm.
+    '''
+    n = len(Li)
+    if n < 2:
+        return Li
+    # divide
+    mid = n // 2
+    L1 = Li[0:mid]
+    L2 = Li[mid:n]
+    # conquer (with recursion)
+    merge_sort(L1)
+    merge_sort(L2)
+    # merge results
+    merge(L1, L2, Li)
+
+
 def find_peak(list_of_integers):
     ''' function that finds a peak in a list of unsorted integers '''
-    _len = len(list_of_integers)
-    if _len == 0:
+    if not list_of_integers:
         return None
-    if _len == 1:
-        return list_of_integers[0]
-    if list_of_integers[0] > list_of_integers[_len - 1]:
-        del list_of_integers[_len - 1]
-    else:
-        del list_of_integers[0]
-    return find_peak(list_of_integers)
+    merge_sort(list_of_integers)
+    return list_of_integers[-1]
