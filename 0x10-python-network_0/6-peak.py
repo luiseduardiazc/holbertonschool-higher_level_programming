bdb6b89e7925443100643471c6342d6e8c50fc35
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-06 23:24:50 -0500

    new implementation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,5 +7,10 @@ def find_peak(list_of_integers):
     _len = len(list_of_integers)
     if _len == 0:
         return None
-    list_of_integers.sort()
-    return list_of_integers[_len - 1]
+    if _len == 1:
+        return list_of_integers[0]
+    if list_of_integers[0] > list_of_integers[_len - 1]:
+        del list_of_integers[_len - 1]
+    else:
+        del list_of_integers[0]
+    return find_peak(list_of_integers)
