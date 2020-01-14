8ce6b7252b91ce3fd89d2ce62a2a6e68764758ac
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-13 19:22:02 -0500

    fixed pycodestyle

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-Write a Python script that takes your Github credentials (username and password)
+Write a Python script that takes your Github
+credentials (username and password)
 and uses the Github API to display your id
 '''
 
