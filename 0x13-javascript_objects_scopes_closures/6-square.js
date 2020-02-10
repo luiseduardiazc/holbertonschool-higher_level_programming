bb1cd8cf12804ccef06aaff86e738bb1c0369d09
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-02-10 17:09:30 -0500

    add task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let x = 0; x < this.height; x++) {
+        console.log('C'.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
