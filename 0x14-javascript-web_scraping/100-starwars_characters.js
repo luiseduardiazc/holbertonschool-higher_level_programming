e040acfd191f918038195e784e799439ec364bfc
Luis Eduardo <luis.diaz.car@gmail.com>
2020-02-11 15:33:28 -0500

    add star wars task

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movie = process.argv[2];
+const url = 'https://swapi.co/api/films/' + movie;
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  const film = JSON.parse(body);
+  for (const character of film.characters) {
+    request(character, function (err, res, body) {
+      if (err) {
+        console.log(err);
+      }
+      const chr = JSON.parse(body);
+      console.log(chr.name);
+    });
+  }
+});
