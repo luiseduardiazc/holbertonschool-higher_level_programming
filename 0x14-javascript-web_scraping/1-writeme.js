bc5b846b2f416e34fd3963ecd2e5f6d5397936f6
Luis Eduardo <luis.diaz.car@gmail.com>
2020-02-11 13:58:12 -0500

    add comments

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-
+// script that writes a string to a file.
 const fs = require('fs');
 const argv = process.argv;
-let filePath = argv[2];
-let string = argv[3];
+const filePath = argv[2];
+const string = argv[3];
 
 fs.writeFile(filePath, string, 'utf8', function (err) {
   if (err) {
