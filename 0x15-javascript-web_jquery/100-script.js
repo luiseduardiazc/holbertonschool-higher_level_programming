d6151c01b77e9de3716ae6dbd70ee1aed8b2c2fd
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-02-16 17:29:08 -0500

    add task to project

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// check if document is ready
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('header').style.color = '#FF0000';
+  }, false);
+})();
